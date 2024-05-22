a45ee22dfb791d20ae15ae93cbb3f4f6eb0f4623
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2024-05-22 15:15:55 +0300

    fix: changed tables data
    + feat: loaded team's images

--- FMKLApp/FMKLApp/Main/Views/GamesStatsTableView.swift
+++ FMKLApp/FMKLApp/Main/Views/GamesStatsTableView.swift
@@ -81,12 +81,12 @@ extension GamesStatsTableView {
     func fetchGroup1() {
         self.teamStats = [
             TeamGamesStats(points: 18, teamName: "BUSUS", wins: 6, loses: 6, goalsScored: 37, goalsMissed: 19),
-            TeamGamesStats(points: 15, teamName: "MEMPH", wins: 6, loses: 5, goalsScored: 51, goalsMissed: 8 ),
-            TeamGamesStats(points: 10, teamName: "ZVZD" , wins: 6, loses: 3, goalsScored: 31, goalsMissed: 25),
-            TeamGamesStats(points: 10, teamName: "XTRM" , wins: 6, loses: 3, goalsScored: 22, goalsMissed: 29),
-            TeamGamesStats(points: 6 , teamName: "FTR"  , wins: 6, loses: 2, goalsScored: 26, goalsMissed: 27),
-            TeamGamesStats(points: 3 , teamName: "AVGRS", wins: 6, loses: 1, goalsScored: 33, goalsMissed: 66),
-            TeamGamesStats(points: 0 , teamName: "KFC"  , wins: 6, loses: 0, goalsScored: 23, goalsMissed: 49)
+            TeamGamesStats(points: 15, teamName: "KUBN" , wins: 6, loses: 5, goalsScored: 51, goalsMissed: 8 ),
+            TeamGamesStats(points: 10, teamName: "MEMPH", wins: 6, loses: 3, goalsScored: 31, goalsMissed: 25),
+            TeamGamesStats(points: 10, teamName: "CSAK" , wins: 6, loses: 3, goalsScored: 22, goalsMissed: 29),
+            TeamGamesStats(points: 6 , teamName: "MNTLT", wins: 6, loses: 2, goalsScored: 26, goalsMissed: 27),
+            TeamGamesStats(points: 3 , teamName: "NIKA" , wins: 6, loses: 1, goalsScored: 33, goalsMissed: 66),
+            TeamGamesStats(points: 0 , teamName: "ALGRS", wins: 6, loses: 0, goalsScored: 23, goalsMissed: 49)
         ]
         
         DispatchQueue.main.async {
@@ -96,13 +96,12 @@ extension GamesStatsTableView {
     
     func fetchGroup2() {
         self.teamStats = [
-            TeamGamesStats(points: 17, teamName: "KUBN"  , wins: 6, loses: 6, goalsScored: 46, goalsMissed: 16),
-            TeamGamesStats(points: 10, teamName: "VSPRST", wins: 6, loses: 3, goalsScored: 35, goalsMissed: 29),
-            TeamGamesStats(points: 9 , teamName: "WNNRS" , wins: 6, loses: 3, goalsScored: 25, goalsMissed: 28),
-            TeamGamesStats(points: 6 , teamName: "ALGRS" , wins: 6, loses: 2, goalsScored: 27, goalsMissed: 30),
-            TeamGamesStats(points: 5 , teamName: "BEER"  , wins: 6, loses: 1, goalsScored: 31, goalsMissed: 26),
-            TeamGamesStats(points: 5 , teamName: "CEMUN" , wins: 6, loses: 2, goalsScored: 21, goalsMissed: 37),
-            TeamGamesStats(points: 4 , teamName: "OLMP"  , wins: 5, loses: 1, goalsScored: 23, goalsMissed: 37)
+            TeamGamesStats(points: 17, teamName: "VSPRST", wins: 6, loses: 6, goalsScored: 46, goalsMissed: 16),
+            TeamGamesStats(points: 10, teamName: "GLDBS" , wins: 6, loses: 3, goalsScored: 35, goalsMissed: 29),
+            TeamGamesStats(points: 9 , teamName: "ZVZD"  , wins: 6, loses: 3, goalsScored: 25, goalsMissed: 28),
+            TeamGamesStats(points: 6 , teamName: "NALAK" , wins: 6, loses: 2, goalsScored: 27, goalsMissed: 30),
+            TeamGamesStats(points: 5 , teamName: "CEMUN" , wins: 6, loses: 1, goalsScored: 31, goalsMissed: 26),
+            TeamGamesStats(points: 5 , teamName: "KFC"   , wins: 6, loses: 2, goalsScored: 21, goalsMissed: 37)
         ]
         
         DispatchQueue.main.async {
